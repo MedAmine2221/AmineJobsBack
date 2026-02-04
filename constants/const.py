666243883cde2eb3cd895692c7c43f898bb4e0fd
MedAmine2221 <lazregamine258@gmail.com>
2026-02-04 10:27:56 +0100

    changes

--- constants/const.py
+++ constants/const.py
@@ -1,4 +1,5 @@
 links = [
     "https://www.tunisietravail.net/page/",
-    "https://www.tunijobs.com/jobs?page="
+    "https://www.tunijobs.com/jobs?page=",
+    "https://www.keejob.com/offres-emploi/?page="
 ]
